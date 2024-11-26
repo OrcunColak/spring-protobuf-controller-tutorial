94c8d2904d6ad9eb898f717877a69e0d76b4f130
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-26 16:08:37 +0300

    add new test

--- src/test/java/com/colak/springtutorial/controller/PersonControllerTest.java
+++ src/test/java/com/colak/springtutorial/controller/PersonControllerTest.java
@@ -1,13 +1,16 @@
 package com.colak.springtutorial.controller;
 
 import com.colak.dto.Person;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @WebMvcTest(PersonController.class)
 public class PersonControllerTest {
@@ -16,7 +19,7 @@ public class PersonControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetPerson() throws Exception {
+    public void testGetPersonCompareBytes() throws Exception {
         // Create a sample Person Protobuf object
         Person person = Person.newBuilder()
                 .setName("John Doe")
@@ -31,5 +34,23 @@ public class PersonControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/x-protobuf;charset=UTF-8"))  // Check Content-Type header
                 .andExpect(MockMvcResultMatchers.content().bytes(protobufData));  // Check if the response body matches the expected Protobuf data
+    }
+
+    @Test
+    public void testGetPersonParseFrom() throws Exception {
+        // Perform the GET request and capture the response
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/person/get"))
+                .andReturn();
+
+        // Get the response body as a byte array
+        MockHttpServletResponse servletResponse = result.getResponse();
+        byte[] responseBytes = servletResponse.getContentAsByteArray();
+
+        // Deserialize the Protobuf response into a Person object
+        Person person = Person.parseFrom(responseBytes);
+
+        // Assert that the deserialized data matches the expected values
+        assertEquals("John Doe", person.getName());
+        assertEquals(30, person.getAge());
     }
 }
